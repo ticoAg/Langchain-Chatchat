b1f48aeb539caf7761049a552444ba3f16bffe49
ticoAg <1627635056@qq.com>
2023-11-24 09:39:51 +0800

    .

--- configs/server_config.py
+++ configs/server_config.py
@@ -16,11 +16,11 @@ HTTPX_DEFAULT_TIMEOUT = 300.0
 
 # API 是否开启跨域，默认为False，如果需要开启，请设置为True
 # is open cross domain
-OPEN_CROSS_DOMAIN = False
+OPEN_CROSS_DOMAIN = True
 
 # 各服务器默认绑定host。如改为"0.0.0.0"需要修改下方所有XX_SERVER的host
-DEFAULT_BIND_HOST = "0.0.0.0" if sys.platform != "win32" else "127.0.0.1"
-# DEFAULT_BIND_HOST = "10.228.67.99"
+# DEFAULT_BIND_HOST = "0.0.0.0" if sys.platform != "win32" else "127.0.0.1"
+DEFAULT_BIND_HOST = "10.228.67.99"
 
 # webui.py server
 WEBUI_SERVER = {
