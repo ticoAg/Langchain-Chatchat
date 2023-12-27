c9aa4a24cb892b7a7ae5dea9c8236f6f02b1d0f1
ticoAg <1627635056@qq.com>
2023-12-27 15:01:01 +0800

    update EMBEDDING_MODEL

--- configs/model_config.py
+++ configs/model_config.py
@@ -6,7 +6,7 @@ import os
 MODEL_ROOT_PATH = ""
 
 # 选用的 Embedding 名称
-EMBEDDING_MODEL = "bge-large-zh"
+EMBEDDING_MODEL = "m3e-small"
 
 # Embedding 模型运行设备。设为"auto"会自动检测，也可手动设定为"cuda","mps","cpu"其中之一。
 EMBEDDING_DEVICE = "cuda"
