1d3e0c95a554e8aa2c38370566e81e47a163ea44
ticoAg <1627635056@qq.com>
2024-06-04 15:20:28 +0800

    some logger optim

--- configs/basic_config.py
+++ configs/basic_config.py
@@ -1,9 +1,12 @@
+import logging
 import os
 import shutil
 import sys
 import tempfile
 from functools import partial
 from pathlib import Path
+from types import FrameType
+from typing import cast
 
 import langchain
 from loguru import logger
@@ -16,8 +19,8 @@ log_verbose = False
 langchain.verbose = False
 
 # 通常情况下不需要更改以下内容
-console_level = "TRACE"
-file_level = "DEBUG"
+console_log_level = "TRACE"
+file_log_level = "DEBUG"
 
 # 日志存储路径
 # 日志存储路径
@@ -40,30 +43,78 @@ class Logger:
 
     def __init__(self, appid):
         # 日志格式
-        LOG_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{file.path}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+        CONSOLE_LOG_FORMAT = (
+            "<green>{time:YYYYMMDD HH:mm:ss}</green> | "  # 颜色>时间
+            "{process.name} | "  # 进程名
+            "{thread.name} | "  # 进程名
+            "<cyan>{file.path}</cyan>:<cyan>{line}</cyan> | "  # 行号
+            "<level>{level}</level>: "  # 等级
+            "<level>{message}</level>"  # 日志内容
+        )
 
         logger.add(
-            sink=sys.stderr,
-            level=console_level,
-            format=LOG_FORMAT,
-            backtrace=True,
-            diagnose=True,
+            sink=sys.stdout,
+            level=console_log_level,
+            format=CONSOLE_LOG_FORMAT,
+            # backtrace=True,
+            # diagnose=True,
             filter=partial(filter_function, appid),
+        )
+        FILE_LOG_FORMAT = (
+            "{time:YYYYMMDD HH:mm:ss} - "  # 时间
+            "{process.name} | "  # 进程名
+            "{thread.name} | "  # 进程名
+            "{module}.{function}:{line} - {level} - {message}"  # 模块名.方法名:行号
         )
         logger.add(
             sink=LOG_PATH / f"{appid}.log",
-            level=file_level,
-            format=LOG_FORMAT,
-            rotation="50 MB",
-            colorize=False,
-            encoding="utf-8",
-            backtrace=True,
-            diagnose=True,
-        )  # 文件输出，文件大小超过50MB时滚动
+            level=file_log_level,
+            format=FILE_LOG_FORMAT,
+            retention="7 days",  # 设置历史保留时长
+            backtrace=True,  # 回溯
+            diagnose=True,  # 诊断
+            # enqueue=True,  # 异步写入
+            rotation="10 MB",
+            filter=partial(filter_function, appid),
+        )  # 文件输出，文件大小超过10 MB时滚动
         self.logger = logger
 
+    def init_config(self):
+        LOGGER_NAMES = ("uvicorn.asgi", "uvicorn.access", "uvicorn")
 
-logger = Logger("langchain-chatchat").logger
+        # change handler for default uvicorn logger
+        logging.getLogger().handlers = [InterceptHandler()]
+        for logger_name in LOGGER_NAMES:
+            logging_logger = logging.getLogger(logger_name)
+            logging_logger.handlers = [InterceptHandler()]
+
+    def get_logger(self):
+        return self.logger
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:  # pragma: no cover
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = str(record.levelno)
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:  # noqa: WPS609
+            frame = cast(FrameType, frame.f_back)
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level,
+            record.getMessage(),
+        )
+
+
+_logger = Logger("langchain-chatchat")
+# _logger.init_config()
+logger = _logger.get_logger()
 
 # 临时文件目录，主要用于文件对话
 BASE_TEMP_DIR = os.path.join(tempfile.gettempdir(), "chatchat")
